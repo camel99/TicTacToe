68665b1479363e4de69cb8a41afe7bf90d1f1015
Maciej Kamela <maciej.kamela@pearson.com>
2015-09-03 13:59:25 +0200

    Alghoritm changes

--- assets/js/boardGame.js
+++ assets/js/boardGame.js
@@ -68,35 +68,49 @@ app.boardGame = (function () {
         },
         $gameBtn: $('.game-btn'),
         collectResultButtons: function () {
+
+            var resultButtons = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+            /*
             var resultButtons = {
                     rows: [
-                        //[0, 1, 2], [3, 4, 5], [6, 7, 8]
-                        [this.$gameBtn.eq(0).attr('data-state'), this.$gameBtn.eq(1).attr('data-state'), this.$gameBtn.eq(2).attr('data-state')],
-                        [this.$gameBtn.eq(3).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(5).attr('data-state')],
-                        [this.$gameBtn.eq(6).attr('data-state'), this.$gameBtn.eq(7).attr('data-state'), this.$gameBtn.eq(8).attr('data-state')]
+                        [0, 1, 2], [3, 4, 5], [6, 7, 8]
+                        //[this.$gameBtn.eq(0).attr('data-state'), this.$gameBtn.eq(1).attr('data-state'), this.$gameBtn.eq(2).attr('data-state')],
+                        //[this.$gameBtn.eq(3).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(5).attr('data-state')],
+                        //[this.$gameBtn.eq(6).attr('data-state'), this.$gameBtn.eq(7).attr('data-state'), this.$gameBtn.eq(8).attr('data-state')]
                     ],
                     columns: [
-                        //[0, 3, 6], [1, 4, 7], [2, 5, 8]
-                        [this.$gameBtn.eq(0).attr('data-state'), this.$gameBtn.eq(3).attr('data-state'), this.$gameBtn.eq(6).attr('data-state')],
-                        [this.$gameBtn.eq(1).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(7).attr('data-state')],
-                        [this.$gameBtn.eq(2).attr('data-state'), this.$gameBtn.eq(5).attr('data-state'), this.$gameBtn.eq(8).attr('data-state')]
+                        [0, 3, 6], [1, 4, 7], [2, 5, 8]
+                        //[this.$gameBtn.eq(0).attr('data-state'), this.$gameBtn.eq(3).attr('data-state'), this.$gameBtn.eq(6).attr('data-state')],
+                        //[this.$gameBtn.eq(1).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(7).attr('data-state')],
+                        //[this.$gameBtn.eq(2).attr('data-state'), this.$gameBtn.eq(5).attr('data-state'), this.$gameBtn.eq(8).attr('data-state')]
                     ],
                     biases: [
-                        //[0, 4, 8], [2, 4, 6]
-                        [this.$gameBtn.eq(0).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(8).attr('data-state')],
-                        [this.$gameBtn.eq(2).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(6).attr('data-state')]
+                        [0, 4, 8], [2, 4, 6]
+                        //[this.$gameBtn.eq(0).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(8).attr('data-state')],
+                        //[this.$gameBtn.eq(2).attr('data-state'), this.$gameBtn.eq(4).attr('data-state'), this.$gameBtn.eq(6).attr('data-state')]
                     ]
             };
+            */
             return resultButtons;
         },
         gameOutcome: function () {
             var self = this,
                 resultBtns = self.collectResultButtons();
-            for( var x in resultBtns){
-                for(var j = 0; j < resultBtns[x].length; j++) {
-                    console.log(j);
-                    console.log(x + "-----" + resultBtns[x][j]);
+            this.$gameBtn = $('.game-btn');
+            for( var x = 0; x < resultBtns.length; x++){
+                console.log(this.$gameBtn, resultBtns[x][0], this.$gameBtn.eq(resultBtns[x][0]), this.$gameBtn.eq(resultBtns[x][0]).attr('data-state'));
+                if (this.$gameBtn.eq(resultBtns[x][0]).attr('data-state') == 'X' && this.$gameBtn.eq(resultBtns[x][1]).attr('data-state') == 'X'
+                    && this.$gameBtn.eq(resultBtns[x][2]).attr('data-state') == 'X'){
+                    alert('robert');
+
                 }
+
+
+                //for(var j = 0; j < resultBtns[x].length; j++) {
+                //    console.log(j);
+                //    console.log(x + "-----" + resultBtns[x][j]);
+                //}
             }
             //console.log(self.collectResultButtons());
         }
