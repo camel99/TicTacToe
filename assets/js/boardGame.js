18adc7570236154592626d78ffa5a7e8d66fccf3
Maciej Kamela <maciej.kamela@pearson.com>
2015-09-07 19:45:52 +0200

    Amending winning alghoritm and validating players' names

--- assets/js/boardGame.js
+++ assets/js/boardGame.js
@@ -1,103 +1,115 @@
 var app = app || {};
 
 app.boardGame = (function () {
-    return {
-        init: function () {
-            this.bindNewGame();
-            //this.setDataState('button');
-            this.setNewMove('.game-btn', 'clicked');
-        },
-        /** Starting new game, displaying players' form, clearing local storage and setting one player as a default choice */
-        bindNewGame: function () {
-            var startGameBtn = $('.start-new-game-btn'),
-                self = this;
-            startGameBtn.on('click', function () {
-                self.startNewGame();
+        return {
+            init: function () {
+                this.bindNewGame();
+                //this.setDataState('button');
+                this.setNewMove('.game-btn', 'clicked');
+            },
+            /** Starting new game, displaying players' form, clearing local storage and setting one player as a default choice */
+            bindNewGame: function () {
+                var startGameBtn = $('.start-new-game-btn'),
+                    self = this;
+                startGameBtn.on('click', function () {
+                    self.startNewGame();
 
-            });
-        },
-        startNewGame: function() {
-            var self = this;
-            app.formGame.hideGameBoard();
-            app.storeItems.removeItemsFromLocalStorage('players');
-            app.formGame.clearInputBoxes();
-            app.formGame.setDefaultPlayer();
-            self.clearBoardField();
-        },
-        setNewMove: function (what, newText) {
-            var self = this;
-            var allBtn = $('.not-used');
-            console.log(allBtn.length);
-            $(what).on('click', function () {
-                if ($(this).hasClass('used')) {
-                    alert("Field was used");
-                } else {
-                    $(this).text(newText);
-                    $(this).toggleClass('not-used used');
-                    //$(this).addClass('X');
-                    $(this).attr('data-state', 'X');
+                });
+            },
+            startNewGame: function () {
+                var self = this;
+                self.clearBoardField();
+                app.storeItems.removeItemsFromLocalStorage('players');
+                app.formGame.clearInputBoxes();
+                app.formGame.setDefaultPlayer();
+                app.formGame.hideGameBoard();
+            },
+            setNewMove: function (what, newText) {
+                var self = this;
+                var allBtn = $('.not-used');
+                console.log(allBtn.length);
+                $(what).on('click', function () {
+                    if ($(this).hasClass('used')) {
+                        alert("Field was used");
+                    } else {
+                        $(this).text(newText);
+                        $(this).toggleClass('not-used used');
+                        $(this).attr('data-state', 'X');
+                       if(self.gameOutcome() === true){
+                           return;
+                       }else {
+                           self.computerMove();
+                       }
+                    }
+                })
+            },
+            computerMove: function () {
+                var allBtn = $('.not-used'),
+                    random = Math.floor(Math.random() * allBtn.length),
+                    button = $('.game-btn.not-used'),
+                    chosenBtn = button.eq(random),
+                    self = this;
+                if (chosenBtn.hasClass('used')) {
+                    while (chosenBtn.hasClass('used')) {
+                        allBtn = $('.not-used');
+                        random = Math.floor(Math.random() * allBtn.length);
+                        chosenBtn = button.eq(random);
+                    }
+                    chosenBtn.text('computer');
+                    chosenBtn.removeClass('not-used');
+                    chosenBtn.addClass('used');
+                    chosenBtn.attr('data-state', 'O');
                     self.gameOutcome();
-                    self.computerMove();
+                } else {
+                    chosenBtn.text('computer');
+                    chosenBtn.removeClass('not-used');
+                    chosenBtn.addClass('used');
+                    chosenBtn.attr('data-state', 'O');
+                    self.gameOutcome();
                 }
-            })
-        },
-        computerMove: function () {
-            var allBtn = $('.not-used'),
-                random = Math.floor(Math.random() * allBtn.length),
-                button = $('.game-btn.not-used'),
-                chosenBtn = button.eq(random);
-            if (chosenBtn.hasClass('used')) {
-                while (chosenBtn.hasClass('used')) {
-                    allBtn = $('.not-used');
-                    random = Math.floor(Math.random() * allBtn.length);
-                    chosenBtn = button.eq(random);
+            },
+            clearBoardField: function () {
+                var gameFields = $('.game-btn'),
+                    gameFieldsAmount = gameFields.length;
+                console.log(gameFields);
+                for (var i = 0; i < gameFieldsAmount; i++) {
+                    gameFields.removeClass('used');
+                    gameFields.addClass('not-used');
+                    gameFields.removeAttr('data-state');
+                    gameFields.text('XO');
                 }
-                chosenBtn.text('computer');
-                chosenBtn.removeClass('not-used');
-                chosenBtn.addClass('used');
-                chosenBtn.attr('data-state', 'O');
-            } else {
-                chosenBtn.text('computer');
-                chosenBtn.removeClass('not-used');
-                chosenBtn.addClass('used');
-                chosenBtn.attr('data-state', 'O');
-            }
-        },
-        clearBoardField: function () {
-            var gameFields = $('.used');
-            gameFields.removeClass('used');
-            gameFields.removeAttr('data-state');
-            gameFields.addClass('not-used');
-            gameFields.text('');
-        },
-        $gameBtn: $('.game-btn'),
-        collectResultButtons: function () {
+            },
+            $gameBtn: $('.game-btn'),
+            collectResultButtons: function () {
 
-            var resultButtons = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-            return resultButtons;
-        },
-        gameOutcome: function () {
-            var self = this,
-                resultBtns = self.collectResultButtons();
-            this.$gameBtn = $('.game-btn');
-            for (var x = 0; x < resultBtns.length; x++) {
-                if (self.getBtnDataState(x, 0) == 'X' && self.getBtnDataState(x, 1) == 'X' && self.getBtnDataState(x, 2) == 'X') {
-                    alert('Player won!');
-                    self.startNewGame();
+                var resultButtons = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+                return resultButtons;
+            },
+            gameOutcome: function () {
+                var self = this,
+                    resultBtns = self.collectResultButtons();
+                this.$gameBtn = $('.game-btn');
+                for (var x = 0; x < resultBtns.length; x++) {
+                    if (self.getBtnDataState(x, 0) == 'X' && self.getBtnDataState(x, 1) == 'X' && self.getBtnDataState(x, 2) == 'X') {
+                        self.playerWon();
+                        self.startNewGame();
+                        return true;
+                    }
+                    if (self.getBtnDataState(x, 0) == 'O' && self.getBtnDataState(x, 1) == 'O' && self.getBtnDataState(x, 2) == 'O') {
+                        alert('Computer won!');
+                        self.startNewGame();
+                    }
                 }
-                if (self.getBtnDataState(x, 0) == 'O' && self.getBtnDataState(x, 1) == 'O' && self.getBtnDataState(x, 2) == 'O') {
-                    console.log(self.getBtnDataState(x, 0), self.getBtnDataState(x, 1), self.getBtnDataState(x, 2));
-                    alert('Computer won!');
-                    self.startNewGame();
-                }
+            },
+            getBtnDataState: function (arrayNo, elemNo) {
+                var self = this,
+                    resultBtns = self.collectResultButtons();
+                return this.$gameBtn.eq(resultBtns[arrayNo][elemNo]).attr('data-state');
+            },
+            playerWon: function () {
+                alert("player won");
             }
-        },
-        getBtnDataState: function (arrayNo, elemNo) {
-            var self = this,
-                resultBtns = self.collectResultButtons();
 
-            return this.$gameBtn.eq(resultBtns[arrayNo][elemNo]).attr('data-state');
         }
-    }
-}()
+    }()
 );
--- assets/js/formGame.js
+++ assets/js/formGame.js
@@ -1,122 +1,130 @@
 var app = app || {};
 
 app.formGame = (function () {
-    return {
-        init: function () {
-            this.startTime();
-            this.runGame();
-            this.enableSecondPlayerName();
-            this.hideSecondPlayer();
-            this.disableFormSubmit();
+        return {
+            init: function () {
+                this.startTime();
+                this.runGame();
+                this.enableSecondPlayerName();
+                this.hideSecondPlayer();
+                this.disableFormSubmit();
+            },
+            parameters: {
+                $gameBoard: $('.game-board'),
+                $gameForm: $('.game-form')
+            },
+            /** Disable form sending */
+            disableFormSubmit: function () {
+                $("form").submit(function (e) {
+                    e.preventDefault();
+                });
+            },
+            /** Displaying current time */
+            startTime: function () {
+                var today = new Date(),
+                    hour = today.getHours(),
+                    minute = today.getMinutes(),
+                    second = today.getSeconds();
+                hour = (hour < 10 ? "0" : "") + hour;
+                minute = (minute < 10 ? "0" : "") + minute;
+                second = (second < 10 ? "0" : "") + second;
+                var currentTime = hour + ":" + minute + ":" + second;
+                $("#currentTime").text(currentTime);
+            },
+            /** Saving players' names in local storage */
+            savePlayersName: function () {
+                app.storeItems.saveInLocalStorage();
+            },
+            /** Running new game, saving players in local storage and displaying game board*/
+            runGame: function () {
+                var run = $(".run-game-btn"),
+                    self = this;
+                run.on('click', function () {
+                    //app.storeItems.removeItemsFromLocalStorage('players');
+                    if (self.validatePlayersNames() === true) {
+                        return;
+                    } else {
+                        self.savePlayersName();
+                        self.showGameBoard();
+                        app.storeItems.retrieveFromLocalStorage();
+                    }
 
-            //this.parameters.$gameView = $('.game-view');
-            //this.parameters.$gameView = $('.game-form');
-        },
-        parameters: {
-            $gameBoard: $('.game-board'),
-            $gameForm: $('.game-form')
-        },
-        /** Disable form sending */
-        disableFormSubmit: function () {
-            $("form").submit(function (e) {
-                e.preventDefault();
-            });
-        },
-        /** Displaying current time */
-        startTime: function () {
-            var today = new Date(),
-                hour = today.getHours(),
-                minute = today.getMinutes(),
-                second = today.getSeconds();
-            hour = (hour < 10 ? "0" : "") + hour;
-            minute = (minute < 10 ? "0" : "") + minute;
-            second = (second < 10 ? "0" : "") + second;
-            var currentTime = hour + ":" + minute + ":" + second;
-            $("#currentTime").text(currentTime);
-        },
-        /** Saving players' names in local storage */
-        savePlayersName: function () {
-            app.storeItems.saveInLocalStorage();
-        },
-        /** Running new game, saving players in local storage and displaying game board*/
-        runGame: function () {
-            var run = $(".run-game-btn"),
-                self = this;
-            run.on('click', function () {
-                app.storeItems.removeItemsFromLocalStorage('players');
-                //self.validatePlayersNames();
-                self.savePlayersName();
-                self.showGameBoard();
-                app.storeItems.retrieveFromLocalStorage();
-
-            });
-        },
-        /** Displaying second player and setting checked attribute for second player radio button */
-        enableSecondPlayerName: function () {
-            var secondPlayer = $('.second-player'),
-                self = this;
-            secondPlayer.on('click', function () {
-                $('.second-player-input').attr("style", "display: block");
-                self.setCheckedAttribute(this);
-                self.removeCheckedAttribute('.first-player');
-            });
-        },
-        /** Hiding second player text box */
-        hideSecondPlayer: function () {
-            var firstPlayer = $('.first-player'),
-                self = this;
-            firstPlayer.on('click', function () {
-                $('.second-player-input').attr("style", "display: none");
-                self.setCheckedAttribute(this);
-                self.removeCheckedAttribute('.second-player');
-            })
-        },
-        /** Showing game board and hiding form view */
-        showGameBoard: function () {
-            if (this.parameters.$gameBoard.is(':hidden')) {
-                this.parameters.$gameBoard.attr("style", "display: block");
-                this.parameters.$gameForm.attr("style", "display: none");
-            }
-        },
-        /** Hiding game board and showing game view */
-        hideGameBoard: function () {
-            if (this.parameters.$gameForm.is(':hidden')) {
-                this.parameters.$gameForm.attr("style", "display: block");
-                this.parameters.$gameBoard.attr("style", "display: none");
-            }
-        },
-        /** Clearing input boxes*/
-        clearInputBoxes: function () {
-            $('.player').val("");
-        },
-        /** Setting one player as a default choice */
-        setDefaultPlayer: function () {
-            $('.first-player').click();
-        },
-        /** Setting checked attribute to chosen element */
-        setCheckedAttribute: function (element) {
-            $(element).attr('checked', true);
-        },
-        /** Removing checked attribute from chosen element */
-        removeCheckedAttribute: function (element) {
-            $(element).attr('checked', false);
-        },
-        validatePlayersNames: function () {
-            var firstPlayerName = $('.player-one-name'),
-                secondPlayerName = $('.player-two-name'),
-                secondPlayerRadioBtn = $('.second-player');
-            if (secondPlayerRadioBtn.is(':checked')) {
-                if (firstPlayerName.val().trim().length == 0) {
-                    alert('First player name shouldn\'t be empty');
+                });
+            },
+            /** Displaying second player and setting checked attribute for second player radio button */
+            enableSecondPlayerName: function () {
+                var secondPlayer = $('.second-player-radio-btn'),
+                    self = this;
+                secondPlayer.on('click', function () {
+                    $('.second-player-input').attr("style", "display: block");
+                    self.setCheckedAttribute(this);
+                    self.removeCheckedAttribute('.first-player-radio-btn');
+                });
+            },
+            /** Hiding second playext box */
+            hideSecondPlayer: function () {
+                var firstPlayer = $('.first-player-radio-btn'),
+                    self = this;
+                firstPlayer.on('click', function () {
+                    $('.second-player-input').attr("style", "display: none");
+                    self.setCheckedAttribute(this);
+                    self.removeCheckedAttribute('.second-player-radio-btn');
+                })
+            },
+            /** Showing game board and hiding form view */
+            showGameBoard: function () {
+                if (this.parameters.$gameBoard.is(':hidden')) {
+                    this.parameters.$gameBoard.attr("style", "display: block");
+                    this.parameters.$gameForm.attr("style", "display: none");
                 }
-                if (secondPlayerName.val().trim().length == 0) {
-                    alert('Second player name shouldn\'t be empty');
+            },
+            /** Hiding game board and showing game view */
+            hideGameBoard: function () {
+                if (this.parameters.$gameForm.is(':hidden')) {
+                    this.parameters.$gameForm.attr("style", "display: block");
+                    this.parameters.$gameBoard.attr("style", "display: none");
                 }
-            } else {
-                alert('Please add your name');
+            },
+            /** Clearing input boxes*/
+            clearInputBoxes: function () {
+                $('.player').val("");
+            },
+            /** Setting one player as a default choice */
+            setDefaultPlayer: function () {
+                $('.first-player-radio-btn').click();
+            },
+            /** Setting checked attribute to chosen element */
+            setCheckedAttribute: function (element) {
+                $(element).attr('checked', true);
+            },
+            /** Removing checked attribute from chosen element */
+            removeCheckedAttribute: function (element) {
+                $(element).attr('checked', false);
+            },
+            validatePlayersNames: function () {
+                var self = this,
+                    firstPlayerName = $('.player-one-name'),
+                    secondPlayerName = $('.player-two-name'),
+                    secondPlayerRadioBtn = $('.second-player-radio-btn');
+                if (secondPlayerRadioBtn.is(':checked')) {
+                    if (self.playerNameLength(firstPlayerName) == 0 || self.playerNameLength(secondPlayerName) == 0) {
+                        alert('Please set player\'s name');
+                        return true;
+                    }
+                } else {
+                    if (firstPlayerName.val().trim().length == 0) {
+                        alert('First player name shouldn\'t be empty');
+                        return true;
+                    }
+                }
+            },
+            validateFirstPlayer: function () {
+                $('.firstPlayerName').after(document.createTextNode("Hello"));
+            },
+            playerNameLength: function (player) {
+                return player.val().trim().length
             }
         }
-    }
-}()
+    }()
 )
 ;
