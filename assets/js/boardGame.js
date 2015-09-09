bb7b6a85b2014b8d1c6f2f8209be801d99a70377
Maciej Kamela <maciej.kamela@pearson.com>
2015-09-09 22:50:51 +0200

    Seprating alghoritm from view part I

--- assets/js/boardGame.js
+++ assets/js/boardGame.js
@@ -1,128 +1,150 @@
 var app = app || {};
 
 app.boardGame = (function () {
-    return {
-        init: function () {
-            this.bindNewGame();
-            //this.setDataState('button');
-            //this.setNewMove('.game-btn', 'clicked');
-            this.setNewMove('.game-btn','X')
-        },
-        /** Starting new game, displaying players' form, clearing local storage and setting one player as a default choice */
-        bindNewGame: function () {
-            var startGameBtn = $('.start-new-game-btn'),
-                self = this;
-            startGameBtn.on('click', function () {
-                self.startNewGame();
-
-            });
-        },
-        startNewGame: function () {
-            var self = this;
-            self.clearBoardField();
-            app.storeItems.removeItemsFromLocalStorage('players');
-            app.formGame.clearInputBoxes();
-            app.formGame.setDefaultPlayer();
-            app.formGame.hideGameBoard();
-        },
-        setNewMove: function (what, newText) {
-            var self = this;
-            var allBtn = $('.not-used');
-            console.log(allBtn.length);
-            $(what).on('click', function () {
-                var index = $(what).index(this);
-                app.algorithm.setNewMove(index);
-                $(this).text(newText);
-                $(this).toggleClass('not-used used');
-                $(this).attr('data-state', 'X');
-            })
-        },
-        //setNewMove: function (what, newText) {
-        //    var self = this;
-        //    var allBtn = $('.not-used');
-        //    console.log(allBtn.length);
-        //    $(what).on('click', function () {
-        //        if ($(this).hasClass('used')) {
-        //            alert("Field was used");
-        //        } else {
-        //            $(this).text(newText);
-        //            $(this).toggleClass('not-used used');
-        //            $(this).attr('data-state', 'X');
-        //           if(self.gameOutcome() === true){
-        //               return;
-        //           }else {
-        //               self.computerMove();
-        //           }
-        //        }
-        //    })
-        //},
-        computerMove: function () {
-            var allBtn = $('.not-used'),
-                random = Math.floor(Math.random() * allBtn.length),
-                button = $('.game-btn.not-used'),
-                chosenBtn = button.eq(random),
-                self = this;
-            if (chosenBtn.hasClass('used')) {
-                while (chosenBtn.hasClass('used')) {
-                    allBtn = $('.not-used');
-                    random = Math.floor(Math.random() * allBtn.length);
-                    chosenBtn = button.eq(random);
-                }
-                chosenBtn.text('computer');
-                chosenBtn.removeClass('not-used');
-                chosenBtn.addClass('used');
-                chosenBtn.attr('data-state', 'O');
-                self.gameOutcome();
-            } else {
-                chosenBtn.text('computer');
-                chosenBtn.removeClass('not-used');
-                chosenBtn.addClass('used');
-                chosenBtn.attr('data-state', 'O');
-                self.gameOutcome();
-            }
-        },
-        clearBoardField: function () {
-            var gameFields = $('.game-btn'),
-                gameFieldsAmount = gameFields.length;
-            console.log(gameFields);
-            for (var i = 0; i < gameFieldsAmount; i++) {
-                gameFields.removeClass('used');
-                gameFields.addClass('not-used');
-                gameFields.removeAttr('data-state');
-                gameFields.text('XO');
-            }
-        },
-        $gameBtn: $('.game-btn'),
-        collectResultButtons: function () {
-
-            var resultButtons = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-            return resultButtons;
-        },
-        gameOutcome: function () {
-            var self = this,
-                resultBtns = self.collectResultButtons();
-            this.$gameBtn = $('.game-btn');
-            for (var x = 0; x < resultBtns.length; x++) {
-                if (self.getBtnDataState(x, 0) == 'X' && self.getBtnDataState(x, 1) == 'X' && self.getBtnDataState(x, 2) == 'X') {
-                    self.playerWon();
+        return {
+            init: function () {
+                this.bindNewGame();
+                //this.setDataState('button');
+                //this.setNewMove('.game-btn', 'clicked');
+                this.setNewMove('.game-btn')
+            },
+            /** Starting new game, displaying players' form, clearing local storage and setting one player as a default choice */
+            bindNewGame: function () {
+                var startGameBtn = $('.start-new-game-btn'),
+                    self = this;
+                startGameBtn.on('click', function () {
                     self.startNewGame();
-                    return true;
+
+                });
+            },
+            startNewGame: function () {
+                var self = this;
+                self.clearBoardField();
+                app.storeItems.removeItemsFromLocalStorage('players');
+                app.formGame.clearInputBoxes();
+                app.formGame.setDefaultPlayer();
+                app.formGame.hideGameBoard();
+                app.algorithm.clearGameFields();
+            },
+            setNewMove: function (what) {
+                var self = this;
+                var allBtn = $('.not-used');
+                $(what).on('click', function () {
+                    var index = $(what).index(this);
+                    //app.algorithm.dawajTablice();
+                    if (!app.algorithm.gameFields[index]) {
+                        app.algorithm.playerMove(index);
+                        self.setPlayerFieldProperties(this, 'X');// jak to poprawic
+                        //app.algorithm.dawajTablice();
+                        self.runComputerMove();
+                        //app.algorithm.dawajTablice();
+                    } else {
+                        alert('Field was used');
+                    }
+                })
+            },
+            setPlayerFieldProperties: function (what, newText) {
+                $(what).text(newText);
+                $(what).toggleClass('not-used used');
+                $(what).attr('data-state', newText);
+            },
+            //setNewMove: function (what, newText) {
+            //    var self = this;
+            //    var allBtn = $('.not-used');
+            //    console.log(allBtn.length);
+            //    $(what).on('click', function () {
+            //        if ($(this).hasClass('used')) {
+            //            alert("Field was used");
+            //        } else {
+            //            $(this).text(newText);
+            //            $(this).toggleClass('not-used used');
+            //            $(this).attr('data-state', 'X');
+            //           if(self.gameOutcome() === true){
+            //               return;
+            //           }else {
+            //               self.computerMove();
+            //           }
+            //        }
+            //    })
+            //},
+            runComputerMove: function () {
+                var allBtn = $('.not-used');
+
+                var btnNumber = app.algorithm.computerMove(allBtn);
+                console.log('buton number to ' + btnNumber);
+                var button = $('.game-btn');
+                var chosenBtn = button.eq(btnNumber);
+                chosenBtn.text('O');
+                console.log(chosenBtn);
+
+            },
+            //computerMove: function () {
+            //    var allBtn = $('.not-used'),
+            //        random = Math.floor(Math.random() * allBtn.length),
+            //        button = $('.game-btn.not-used'),
+            //        chosenBtn = button.eq(random),
+            //        self = this;
+            //    if (chosenBtn.hasClass('used')) {
+            //        while (chosenBtn.hasClass('used')) {
+            //            allBtn = $('.not-used');
+            //            random = Math.floor(Math.random() * allBtn.length);
+            //            chosenBtn = button.eq(random);
+            //        }
+            //        chosenBtn.text('computer');
+            //        chosenBtn.removeClass('not-used');
+            //        chosenBtn.addClass('used');
+            //        chosenBtn.attr('data-state', 'O');
+            //        self.gameOutcome();
+            //    } else {
+            //        chosenBtn.text('computer');
+            //        chosenBtn.removeClass('not-used');
+            //        chosenBtn.addClass('used');
+            //        chosenBtn.attr('data-state', 'O');
+            //        self.gameOutcome();
+            //    }
+            //},
+            clearBoardField: function () {
+                var gameFields = $('.game-btn'),
+                    gameFieldsAmount = gameFields.length;
+                console.log(gameFields);
+                for (var i = 0; i < gameFieldsAmount; i++) {
+                    gameFields.removeClass('used');
+                    gameFields.addClass('not-used');
+                    gameFields.removeAttr('data-state');
+                    gameFields.text('XO');
                 }
-                if (self.getBtnDataState(x, 0) == 'O' && self.getBtnDataState(x, 1) == 'O' && self.getBtnDataState(x, 2) == 'O') {
-                    alert('Computer won!');
-                    self.startNewGame();
+            },
+            $gameBtn: $('.game-btn'),
+            collectResultButtons: function () {
+
+                var resultButtons = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+                return resultButtons;
+            },
+            gameOutcome: function () {
+                var self = this,
+                    resultBtns = self.collectResultButtons();
+                this.$gameBtn = $('.game-btn');
+                for (var x = 0; x < resultBtns.length; x++) {
+                    if (self.getBtnDataState(x, 0) == 'X' && self.getBtnDataState(x, 1) == 'X' && self.getBtnDataState(x, 2) == 'X') {
+                        self.playerWon();
+                        self.startNewGame();
+                        return true;
+                    }
+                    if (self.getBtnDataState(x, 0) == 'O' && self.getBtnDataState(x, 1) == 'O' && self.getBtnDataState(x, 2) == 'O') {
+                        alert('Computer won!');
+                        self.startNewGame();
+                    }
                 }
+            },
+            getBtnDataState: function (arrayNo, elemNo) {
+                var self = this,
+                    resultBtns = self.collectResultButtons();
+                return this.$gameBtn.eq(resultBtns[arrayNo][elemNo]).attr('data-state');
+            },
+            playerWon: function () {
+                alert("player won");
             }
-        },
-        getBtnDataState: function (arrayNo, elemNo) {
-            var self = this,
-                resultBtns = self.collectResultButtons();
-            return this.$gameBtn.eq(resultBtns[arrayNo][elemNo]).attr('data-state');
-        },
-        playerWon: function () {
-            alert("player won");
+
         }
-
-    }
-}()
+    }()
 );
--- assets/js/gameAlgorithm.js
+++ assets/js/gameAlgorithm.js
@@ -10,16 +10,43 @@ app.algorithm = (function () {
         push: function (index, mark) {
             this.gameFields[index] = mark;
         },
+        clearGameFields: function(){ // odpalaj na start new game
+            this.gameFields = [];
+        },
         gameOutcome: function () {
             //robisz to samo co na buttonach tylko na tej tablicy
         },
-        setNewMove: function (index) {
-             if (!this.gameFields[index]) {
-                 this.gameFields[index] = 'X';
-                 console.log(this.gameFields);
-             } else {
-                 alert('Field was used');
-             }
+        playerMove: function (index) {
+            if (!this.gameFields[index]) {
+                this.gameFields[index] = 'X';
+                console.log('player index ' + index);
+            }
+        },
+        computerMove: function (elements) {
+            var random  = Math.floor(Math.random() * elements.length);
+            var chosenField = this.gameFields[random];
+            //console.log(random,this.gameFields, chosenField);
+            console.log('random ' + random);
+            if(chosenField){
+                console.log('zajety');
+                console.log(chosenField);
+                while(chosenField){
+                    random  = Math.floor(Math.random() * elements.length);
+                    chosenField = this.gameFields[random];
+                    console.log('new random to ' + random);
+                }
+                this.gameFields[random] = 'O';
+                console.log(random,this.gameFields, chosenField);
+
+                return random;
+            } else {
+                this.gameFields[random] = 'O';
+                console.log('zwrocilem ' + random);
+                return random;
+            }
+        },
+        dawajTablice: function () {
+            console.dir(this.gameFields);
         }
     }
 }()
